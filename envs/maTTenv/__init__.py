3fc3014535ac22fec95ed32a2c0ffaad070231fc
hydrargyrum5799 <hgoel1000p@gmail.com>
2022-08-17 17:08:26 -0400

    attention added

--- envs/maTTenv/__init__.py
+++ envs/maTTenv/__init__.py
@@ -53,7 +53,7 @@ def make(env_name, render=False, figID=0, record=False, directory='',
         env = make_vec_env(lambda: env, n_envs=kwargs["num_envs"], vec_env_cls=gym.vector.SyncVectorEnv)
         env = maTimeLimitVec(env, max_episode_steps=T_steps)
     else:
-        env = make_vec_env(lambda: env, n_envs=1, vec_env_cls=gym.vector.AsyncVectorEnv)
+        env = make_vec_env(lambda: env, n_envs=1, vec_env_cls=gym.vector.SyncVectorEnv)
         env = maTimeLimitVec(env, max_episode_steps=T_steps)
 
     # UNpacked Observation space per agent
